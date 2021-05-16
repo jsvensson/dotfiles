d994acee29685c1a3adbb0a4114a3495ebe1933f
Johan Svensson <johan@reducer.io>
2021-05-16 12:20:49 +0200

    fish: add ll tree aliases; key bindings

---
+++ private_dot_config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,2 @@
+bind \cg fzf_ghq_go_to_repository
+bind \cd fzf_change_directory
---
+++ private_dot_config/fish/functions/fzf_change_directory.fish
@@ -0,0 +1,7 @@
+function fzf_change_directory -d 'Go to local directory'
+	set -l query (commandline -b)
+	[ -n query ]; and set flags --query="$query"; or set flags
+	fd -t d | fzf $flags | read select
+	[ -n "$select" ]; and builtin cd $select
+	commandline -f repaint
+end
