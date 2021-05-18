5dfd948ed211f9c3bd3d0b17ecb2bbe230c9c50a
Johan Svensson <johan.svensson@detectify.com>
2021-05-18 15:28:36 +0200

    fish: add aws_logout func

---
+++ private_dot_config/fish/functions/aws_logout.fish
@@ -0,0 +1,8 @@
+function aws_logout -d 'Log out of aws and aws-vault'
+	# Find all env vars starting with 'AWS_'
+	set tokens (set | grep '^AWS_' | cut -f 1 -d ' ')
+	for t in $tokens
+		set -e $t
+	end
+end
+
