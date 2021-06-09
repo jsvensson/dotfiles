768309d74333d2ee882601315542131c5ae18940
Johan Svensson <johan@reducer.io>
2021-06-09 21:37:00 +0200

    fish/fzf: add git log preview to branch change

--- private_dot_config/fish/functions/fzf_change_git_branch.fish
+++ private_dot_config/fish/functions/fzf_change_git_branch.fish
@@ -1,5 +1,16 @@
+# With inspiration from https://polothy.github.io/post/2019-08-19-fzf-git-checkout/
 function fzf_change_git_branch -d "Change git branch"
-	set branch (git --no-pager branch -vv | fzf +m)
+	if not git rev-parse HEAD > /dev/null 2>&1
+		echo "Not in a git repository"
+		return
+	end
+
+	set -l preview 'git log -n 50 --color=always --date=short --pretty="format:%C(auto)%cd %h%d %s"'
+	set -l branch (git --no-pager branch -vv | fzf +m --preview $preview)
+	if not set -q var[1]
+		return
+	end
+
 	git checkout (echo $branch | awk '{print $1}' | sed "s/.* //")
 end
 
