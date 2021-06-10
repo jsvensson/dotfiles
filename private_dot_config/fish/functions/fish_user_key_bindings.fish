f22f6e434bcc9de596d8ec5290c954571122a7bf
Johan Svensson <johan@reducer.io>
2021-06-10 22:24:25 +0200

    fish/fzf: start reorganizing

--- private_dot_config/fish/functions/fish_user_key_bindings.fish
+++ private_dot_config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,3 @@
-bind \cb fzf_change_git_branch
+bind \cb fzf-git-checkout
 bind \cd fzf_change_directory
 bind \cg fzf_ghq_go_to_repository
--- private_dot_config/fish/functions/fzf-git-checkout.fish
+++ private_dot_config/fish/functions/fzf-git-checkout.fish
@@ -1,5 +1,5 @@
 # With inspiration from https://polothy.github.io/post/2019-08-19-fzf-git-checkout/
-function fzf_change_git_branch -d "Change git branch"
+function fzf-git-checkout -d "Change git branch"
 	if not git rev-parse HEAD > /dev/null 2>&1
 		echo "Not in a git repository"
 		return
