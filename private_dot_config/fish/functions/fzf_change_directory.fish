a8594cb3eed58321b6611af87d4038b4e38efcbe
Johan Svensson <johan.svensson@detectify.com>
2022-04-14 14:38:47 +0200

    fzf: show dir preview on directory change

--- private_dot_config/fish/functions/fzf_change_directory.fish
+++ private_dot_config/fish/functions/fzf_change_directory.fish
@@ -1,7 +1,7 @@
 function fzf_change_directory -d 'Go to local directory'
 	set -l query (commandline -b)
 	[ -n query ]; and set flags --query="$query"; or set flags
-	fd -t d | fzf $flags | read select
+	fd -t d | fzf $flags --preview "ll {}" | read select
 	[ -n "$select" ]; and builtin cd $select
 	commandline -f repaint
 end
