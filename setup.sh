4f302c1b06536967766563cbf2aee5788d52bbff
Johan Svensson <johan@reducer.io>
2023-05-26 11:16:29 +0200

    feat: add setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Setup file to do some initial installation. Requires the brewfile
+# to be applied first so that all tools are available.
+
+# Install tpm for tmux
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
