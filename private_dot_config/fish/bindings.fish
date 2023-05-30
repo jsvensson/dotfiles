79d81243cf6dcc51e1b33aa70beeefc3f318f7fa
Johan Svensson <johan@reducer.io>
2023-05-30 21:16:46 +0200

    chore(fish): fix shift+arrow bindings inside tmux

--- private_dot_config/fish/bindings.fish
+++ private_dot_config/fish/bindings.fish
@@ -1,3 +1,9 @@
+# Fix shift+arrow keys in tmux
+#bind \e\[1\;2A backward-word # shift-up
+#bind \e\[1\;2B backward-word # shift-down
+bind \e\[1\;2D backward-word # shift-left
+bind \e\[1\;2C forward-word # shift-right
+
 # Ctrl-D: fzf, list directories recursively under pwd
 bind \cd fzf_change_directory
 
