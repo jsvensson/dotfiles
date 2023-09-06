00a69d452306b2ff898c0e4649377af741117971
Johan Svensson <johan@reducer.io>
2023-09-06 16:22:32 +0200

    chore(neovim): adjust NormalFloat bg color

--- private_dot_config/nvim/lua/echo/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/echo/plugins/colorscheme.lua
@@ -2,6 +2,14 @@ return {
   "Shatur/neovim-ayu",
   priority = 1000, -- load before all other plugins
   config = function()
+    require("ayu").setup({
+      mirage = false,
+      -- Transparency overrides, see https://github.com/Shatur/neovim-ayu#transparency
+      overrides = {
+        NormalFloat = { bg = "#212733" }, -- lighter UI popups
+      },
+    })
+
     vim.cmd([[colorscheme ayu]])
   end,
 }
