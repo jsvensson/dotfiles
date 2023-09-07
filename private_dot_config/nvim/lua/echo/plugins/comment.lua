e840575575c212a01d1f0022b509f2143f9351cb
Johan Svensson <johan@reducer.io>
2023-09-07 23:58:50 +0200

    chore(neovim): change comment keys

--- private_dot_config/nvim/lua/echo/plugins/comment.lua
+++ private_dot_config/nvim/lua/echo/plugins/comment.lua
@@ -1,8 +1,14 @@
-return {
+local M = {
   "numToStr/Comment.nvim",
   event = {
     "BufReadPre",
     "BufNewFile",
   },
-  config = true -- runs require('Comment').setup()
+  config = true, -- runs require('Comment').setup()
+  keys = {
+    { 'gb', mode = { 'n', 'v' }, desc = "Block comment" },
+    { 'gc', mode = { 'n', 'v' }, desc = "Line comment" },
+  }
 }
+
+return M
