262064eb8fd6aba81ebde8a4ff2f2d34c5cb057c
Johan Svensson <johan.svensson@detectify.com>
2023-09-08 11:52:26 +0200

    neovim: lighter line highlight in neo-tree

--- private_dot_config/nvim/lua/echo/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/echo/plugins/colorscheme.lua
@@ -1,3 +1,5 @@
+local panel_bg = "#212733"
+
 return {
   "Shatur/neovim-ayu",
   priority = 1000, -- load before all other plugins
@@ -5,12 +7,15 @@ return {
     local colors = require("ayu.colors")
     colors.generate()
 
+
+
     require("ayu").setup({
       mirage = false,
       -- Transparency overrides, see https://github.com/Shatur/neovim-ayu#transparency
       overrides = {
         NormalFloat = { bg = "#212733" }, -- lighter UI popups
         LineNr = { fg = colors.comment }, -- lighter number column
+        NeoTreeCursorLine = { bg = panel_bg },
       },
     })
 
