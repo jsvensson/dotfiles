b5c6235bdd92ff5660b3c88da865f8f53ab3fbdc
Johan Svensson <johan.svensson@detectify.com>
2023-09-08 10:51:02 +0200

    neovim/ayu: make number column lighter

--- private_dot_config/nvim/lua/echo/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/echo/plugins/colorscheme.lua
@@ -2,11 +2,15 @@ return {
   "Shatur/neovim-ayu",
   priority = 1000, -- load before all other plugins
   config = function()
+    local colors = require("ayu.colors")
+    colors.generate()
+
     require("ayu").setup({
       mirage = false,
       -- Transparency overrides, see https://github.com/Shatur/neovim-ayu#transparency
       overrides = {
         NormalFloat = { bg = "#212733" }, -- lighter UI popups
+        LineNr = { fg = colors.comment }, -- lighter number column
       },
     })
 
