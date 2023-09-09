f2672e874382c8163be4dd9e3c0e34fb4e6e9ab4
Johan Svensson <johan@reducer.io>
2023-09-09 20:52:17 +0200

    neovim: always show sign column in gutter

--- private_dot_config/nvim/lua/echo/core/options.lua
+++ private_dot_config/nvim/lua/echo/core/options.lua
@@ -7,6 +7,9 @@ opt.termguicolors = true
 opt.relativenumber = true
 opt.number = true -- show absolute line number on current line
 
+-- always show space for diagnostics in gutter
+opt.signcolumn = "yes"
+
 opt.fillchars = { eob = ' ' }
 
 -- tabs/indentation
