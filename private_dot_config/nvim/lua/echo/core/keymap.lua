3829787a975d76d43027c78a08703907763a4ba6
Johan Svensson <johan@reducer.io>
2023-09-10 15:09:07 +0200

    neovim: keymap tweaks

--- private_dot_config/nvim/lua/echo/core/keymap.lua
+++ private_dot_config/nvim/lua/echo/core/keymap.lua
@@ -4,7 +4,11 @@ local keymap = vim.keymap
 vim.g.mapleader = " "
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<Leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<Leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<Leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<Leader>sx", "<Cmd>close<CR>", { desc = "Close current split" })
+
+-- diagnostics
+keymap.set("n", "<Leader>dn", "<Cmd>lua vim.diagnostic.goto_next()<CR>", { desc = "Go to next diagnostic" })
+keymap.set("n", "<Leader>dp", "<Cmd>lua vim.diagnostic.goto_prev()<CR>", { desc = "Go to previous diagnostic" })
