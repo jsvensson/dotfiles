3072fae65c5d382dcb013f680ef8ba7cad79b809
Johan Svensson <johan@reducer.io>
2023-09-10 22:46:55 +0200

    neovim: adjust window border color

--- private_dot_config/nvim/lua/echo/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/echo/plugins/colorscheme.lua
@@ -1,19 +1,17 @@
-local panel_bg = "#212733"
 
 return {
   "Shatur/neovim-ayu",
   priority = 1000, -- load before all other plugins
   config = function()
+    local panel_bg = "#212733"
     local colors = require("ayu.colors")
     colors.generate()
-
-
-
     require("ayu").setup({
       mirage = false,
       -- Transparency overrides, see https://github.com/Shatur/neovim-ayu#transparency
       overrides = {
         NormalFloat = { bg = panel_bg }, -- lighter UI popups
+        WinSeparator = { fg = colors.selection_bg }, -- border between windows
 
         -- adjust line numbers in gutter
         LineNr = { fg = colors.func },
