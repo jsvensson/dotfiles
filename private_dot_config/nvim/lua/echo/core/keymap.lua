d50747628536b1bdc4c576993fdc89795c8ca799
Johan Svensson <johan@reducer.io>
2023-09-10 21:45:25 +0200

    neovim: add diagnostic keys

--- private_dot_config/nvim/lua/echo/core/keymap.lua
+++ private_dot_config/nvim/lua/echo/core/keymap.lua
@@ -3,6 +3,10 @@ local keymap = vim.keymap
 -- set leader key to Space
 vim.g.mapleader = " "
 
+-- copy/paste to system clipboard
+keymap.set({ "n", "v" }, "<Leader>y", "+y", { desc = "Yank to system clipboard" })
+keymap.set("n", "<Leader>p", "+p", { desc = "Put from system clipboard" })
+
 -- window management
 keymap.set("n", "<Leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<Leader>sh", "<C-w>s", { desc = "Split window horizontally" })
