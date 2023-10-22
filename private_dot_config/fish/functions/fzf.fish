66cc949f3628d1135d0172c5f3f81ef31e1d9446
Johan Svensson <johan@reducer.io>
2023-10-22 13:30:39 +0200

    fish/fzf: set var as global, not universal

--- private_dot_config/fish/functions/fzf.fish
+++ private_dot_config/fish/functions/fzf.fish
@@ -1,6 +1,6 @@
 # https://github.com/rose-pine/fzf
 function fzf --wraps="fzf"
-    set -Ux FZF_DEFAULT_OPTS "
+    set -gx FZF_DEFAULT_OPTS "
 	--color=fg:#908caa,bg:#191724,hl:#ebbcba
 	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
 	--color=border:#403d52,header:#31748f,gutter:#191724
