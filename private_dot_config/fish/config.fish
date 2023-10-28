a9091f1d4388a63cb16378e59330d852b1121731
Johan Svensson <johan@reducer.io>
2023-10-28 22:16:50 +0200

    fish: fix fzf.fish history issue

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -31,6 +31,16 @@ set -gx GIT_CONFIG ~/.config/git/config
 set -gx SSH_AUTH_SOCK ~/.1password/agent.sock
 
 
+# fzf config
+set -gx FZF_DEFAULT_OPTS "
+	--color=fg:#908caa,bg:#191724,hl:#ebbcba
+	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+	--color=border:#403d52,header:#31748f,gutter:#191724
+	--color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
+	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa
+	--cycle --layout=reverse-list --border
+	--height=80% --preview-window=wrap --marker='@' --pointer='@'"
+
 ####
 #### Autocompletion definitions
 ####
