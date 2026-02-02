4cb0b939fcbdfd5a67de29557bb1a81e5a9c68a8
Johan Svensson <johan@spiralmind.io>
2026-02-02 22:16:54 +0100

    gitignore: add claude settings; delete old git nav func

--- private_dot_config/fish/functions/fzf_ghq_go_to_repository.fish
+++
@@ -1,8 +0,0 @@
-function fzf_ghq_go_to_repository -d 'Go to repository'
-    set -l query (commandline -b)
-    [ -n query ]
-    ghq list | gum filter --header "Select repository" --height 20 --padding "0 2" | read select
-    [ -n "$select" ]; and builtin cd (ghq root)/$select
-    commandline -r ''
-    commandline -f repaint
-end
