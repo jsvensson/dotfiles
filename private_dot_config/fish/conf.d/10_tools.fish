8277da2afb40715e158cb9f60d0d790186f6b3cf
Johan Svensson <johan@spiralmind.io>
2026-03-02 19:45:06 +0100

    fish: move stuff around

--- private_dot_config/fish/conf.d/10_tools.fish
+++ private_dot_config/fish/conf.d/10_tools.fish
@@ -11,3 +11,9 @@ zoxide init fish | source
 
 # atuin - enhanced shell history with search, sync, and statistics
 atuin init fish | source
+
+# fzf - configure from a file instead of entirely through env vars
+set -gx FZF_DEFAULT_OPTS_FILE ~/.config/fzf/config
+
+# Gum configuration (interactive command-line prompt tool)
+set -gx GUM_FILTER_INDICATOR "❯"
--- private_dot_config/fish/conf.d/60_bindings.fish
+++ private_dot_config/fish/conf.d/60_bindings.fish
@@ -3,5 +3,24 @@
 ####
 # Load custom key bindings and command aliases/abbreviations
 
-source ~/.config/fish/bindings.fish
+# Fix shift+arrow keys in tmux
+#bind \e\[1\;2A backward-word # shift-up
+#bind \e\[1\;2B backward-word # shift-down
+bind \e\[1\;2D backward-word # shift-left
+bind \e\[1\;2C forward-word # shift-right
+
+# Ctrl-D: fzf, list directories recursively under pwd
+bind \cd fzf_change_directory
+
+# Ctrl-G G: Navigate to git repository
+bind \cg\cg git-repo
+
+# Ctrl-G B: check out git branch
+bind \cg\cb fzf-git-checkout
+
+# Ctrl-R: Use Atuin for history search (instead of fzf)
+bind \cr _atuin_search
+
+
+
 source ~/.config/fish/alias.fish
