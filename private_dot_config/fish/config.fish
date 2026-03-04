796bbcf32e5a22347281e3e10dfacf428ae7cdc6
Johan Svensson <johan@spiralmind.io>
2026-03-04 15:27:28 +0100

    fish: add post-config for edge cases

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -7,3 +7,12 @@
 # Theme setup is handled by conf.d/30_theme.fish
 # Tool initialization (starship, zoxide, atuin) by conf.d/10_tools.fish
 # Editor/SSH config handled by conf.d/20_editor.fish
+
+####
+#### Post-config
+#### This is intended for edge cases that need to be set after extensions load.
+
+# Bind Ctrl-R to atuin (overrides fzf's history binding which loads earlier)
+if status is-interactive
+    bind \cr _atuin_search
+end
